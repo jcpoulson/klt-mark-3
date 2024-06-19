72077d35d7e23611f4e1ff46e62502f99ca95a55
Jonah <poulson_jonah@bah.com>
2024-06-19 13:57:44 -0400

    Fixed second build error

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 // @ts-expect-error switch to TS
 import postSaying from "../../backend/postSaying";
 // @ts-expect-error switch to TS
